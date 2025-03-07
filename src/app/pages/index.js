971489babf0d69fdd2c80ce3c6e7ea8e36f29ef6
Rafael Clementino Araujo Soares <rafael.soares@gmail.com>
2025-03-06 22:44:12 -0300

    criando app

---
+++ src/app/pages/index.js
@@ -0,0 +1,49 @@
+import { useState, useEffect } from 'react';
+
+export default function Home() {
+  const [users, setUsers] = useState([]);
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+
+  useEffect(() => {
+    fetch('/api/users')
+      .then((response) => response.json())
+      .then((data) => setUsers(data));
+  }, []);
+
+  const addUser = async () => {
+    const response = await fetch('/api/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ name, email }),
+    });
+    const newUser = await response.json();
+    setUsers([...users, newUser]);
+  };
+
+  return (
+    <div>
+      <h1>Users</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name} - {user.email}</li>
+        ))}
+      </ul>
+      <input
+        type="text"
+        placeholder="Name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <button onClick={addUser}>Add User</button>
+    </div>
+  );
+}
